1258d3f6bd8aebcaa0f66c6817884c4110c464c6
Yihun Alemayehu <yankure01@gmail.com>
2023-12-08 00:38:17 +0300

    forth commit

--- lib/strings.dart
+++ lib/strings.dart
@@ -1,2 +1,13 @@
 const emailOrPasswordEmptyDialogTitle =
     'Please fill int both email and the password fields';
+const emailOrPasswordEmptyDescription =
+    'You seem to have forgotten to enter either the email or the password field, or both';
+const loginErrorDialogTitle = 'LogIn Error';
+const loginErrorDialogContent = 'Invalid email/password combination';
+
+const pleaseWait = 'Please wait...';
+const enterYourPasswordHere = 'Enter your password Here';
+const enterYourEmailHere = 'Enter your email here ';
+const ok = 'OK';
+const login = 'Log in';
+const homePage = 'Home Page';
